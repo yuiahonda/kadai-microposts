1e12face72f1425b67179ab59da26294d0f714c7
yui.honda <yui.a.honda@rakuten.com>
2018-06-06 07:35:13 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
