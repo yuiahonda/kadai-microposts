69da8e857ce1d558dbd91b0a481d9fd72c822986
yui.honda <yui.a.honda@rakuten.com>
2018-06-21 04:46:43 +0000

    kadai-microposts

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -17,8 +17,8 @@
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li><a href="#">My profile</a></li>
-                                <li role="separator" class="divider"></li>
+                                <li>{!! link_to_route('users.show', 'MyProfile', ['id' => Auth::id()]) !!}</a></li>
+                                <li>{!! link_to_route('users.favoritings', 'Favorite', ['id' => Auth::id()]) !!}</li>
                                 <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
                             </ul>
                         </li>
